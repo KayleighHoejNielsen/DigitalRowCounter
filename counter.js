e602c9cc4161d907812e23f333818d7ac7a8649f
Kayleigh Nielsen <purplekitti909@gmail.com>
2025-01-10 14:54:06 +0100

    adding functionality

--- counter.js
+++ counter.js
@@ -1,26 +1,84 @@
 
 let counterValue = 0;
+let counterNumber = 0;
 
 const counterDisplay = document.getElementById("counter-display");
-const incrementButton = document.getElementById("counter-plus");
-const decrementButton = document.getElementById("counter-minus");
+const plusButton = document.getElementById("counter-plus");
+const minusButton = document.getElementById("counter-minus");
 const resetButton = document.getElementById("counter-reset");
+const counter = document.getElementById("counter");
+const newButton = document.getElementById("new-counter")
 
 function updateCounter() {
   counterDisplay.textContent = counterValue;
 }
 
-incrementButton.addEventListener("click", () => {
+plusButton.addEventListener("click", () => {
   counterValue++;
   updateCounter();
 });
 
-decrementButton.addEventListener("click", () => {
+minusButton.addEventListener("click", () => {
   counterValue--;
   updateCounter();
 });
 
 resetButton.addEventListener("click", () => {
-    counterValue = 0;
-    updateCounter();
+  counterValue = 0;
+  updateCounter();
 });
+
+newButton.addEventListener("click", () => {
+  let newCounterValue = 0;
+  let counterId = "counter" + counterNumber;
+  let diplayId = "display" + counterNumber;
+  let plusId = "counter-plus" + counterNumber;
+  let minusId = "counter-minus" + counterNumber;
+  let resetId = "counter-reset" + counterNumber;
+
+  function updateNewCounter() {
+    newCounterDisplay.textContent = newCounterValue;
+  }
+
+  let newCounter = document.createElement("div");
+  newCounter.className = "counter";
+  newCounter.id = counterId;
+
+  let newCounterDisplay = document.createElement("div");
+  newCounterDisplay.className = "counter-display";
+  newCounterDisplay.id = diplayId;
+  newCounterDisplay.textContent = "0";
+  
+  let newPlusButton = document.createElement("button");
+  newPlusButton.className = "button"; 
+  newPlusButton.id = plusId;
+  newPlusButton.textContent = "+";
+
+  newPlusButton.addEventListener("click", () => {
+    newCounterValue++;
+    updateNewCounter();
+  });
+
+  let newMinusButton = document.createElement("button");
+  newMinusButton.className = "button";
+  newMinusButton.id = minusId;
+  newMinusButton.textContent = "-";
+
+  newMinusButton.addEventListener("click", () => {
+    newCounterValue--;
+    updateNewCounter();
+  });
+
+  let newResetButton = document.createElement("button");
+  newResetButton.className = "button";
+  newResetButton.id = resetId;
+  newResetButton.textContent = "Reset";
+
+  newResetButton.addEventListener("click", () => {
+    newCounterValue = 0;
+    updateNewCounter();
+  });
+
+  counterNumber++;
+
+})
